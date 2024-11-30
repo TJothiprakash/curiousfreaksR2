a7cdf1215f101233d546cbb2f7c2f0424674422a
JP <jothiprakash888@gmail.com>
2024-11-30 11:09:19 +0530

    --

---
+++ src/graphs/GraphIntro.java
@@ -0,0 +1,23 @@
+package graphs;
+
+public class GraphIntro {
+
+    // graph and its type
+    // undirected graph
+    // directed graph
+    // weighted graph
+    // weighted undirected graph
+    // weighted directed graph
+    {
+        int num = 10;
+        System.out.println(10);
+    }
+    static {
+        int num = 20;
+        System.out.println(20);
+    }
+
+    public void printGraph() {
+        System.out.println("this will print after 10");
+    }
+}
---
+++ src/graphs/Main.java
@@ -0,0 +1,9 @@
+package graphs;
+
+public class Main {
+    public static void main(String[] args) {
+        GraphIntro graphIntro   = new GraphIntro();
+        graphIntro.printGraph();
+
+    }
+}
