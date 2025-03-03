75b9ba816d4ba0531de708b5fb088d584cac9053
JP <jothiprakash888@gmail.com>
2025-03-03 14:36:17 +0530

    march_3 evening session commited

---
+++ src/collections_practice/streams/Stream_Practice.java
@@ -0,0 +1,13 @@
+public class Stream_Practice {
+    public static void main(String[] args) {
+        //write your code here
+        /*Convert a list of integers to a list of their squares.
+        /*Convert a list of integers to a list of their squares.
+        Filter even numbers from a list of integers.
+        Find the sum of all numbers in a list.
+            Convert a list of strings to uppercase.
+    Find the longest string in a list.
+*/
+
+    }
+}
